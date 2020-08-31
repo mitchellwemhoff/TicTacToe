9d54a061542f581e753941cec9196d37f9d7c3e7
Mitchell Wemhoff <mwemhoff@clearwateranalytics.com>
2020-08-31 16:47:18 -0600

    Added logic for Cat's Game

--- main.py
+++ main.py
@@ -9,43 +9,51 @@ user_input = UserInput()
 def human_goes_first(board):
     while True:
         human_move = user_input.get_input_for_move()
-        human_is_valid_move, is_game_over = board.input_move(human_move)
+        human_is_valid_move, is_game_over, is_cats_game = board.input_move(human_move)
         while not human_is_valid_move:
             print("Invalid move")
             human_move = user_input.get_input_for_move()
-            human_is_valid_move, is_game_over = board.input_move(human_move)
+            human_is_valid_move, is_game_ove, is_cats_game = board.input_move(human_move)
         print(board.board_str)
         if is_game_over:
             print("Game Over")
+        if is_cats_game:
+            print("Cat's Game")
             break
 
         print('AI is thinking...')
         ai_move = ai.calculate_move_genius(board)
-        ai_is_valid_move, is_game_over = board.input_move(ai_move)
+        ai_is_valid_move, is_game_over, is_cats_game = board.input_move(ai_move)
         print(board.board_str)
         if is_game_over:
             print("Game Over")
+        if is_cats_game:
+            print("Cat's Game")
             break
 
 def ai_goes_first(board):
     while True:
         print('AI is thinking...')
         ai_move = ai.calculate_move_genius(board)
-        ai_is_valid_move, is_game_over = board.input_move(ai_move)
+        ai_is_valid_move, is_game_over, is_cats_game = board.input_move(ai_move)
         print(board.board_str)
         if is_game_over:
             print("Game Over")
+        if is_cats_game:
+            print("Cat's Game")
             break
 
         human_move = user_input.get_input_for_move()
-        human_is_valid_move, is_game_over = board.input_move(human_move)
+        human_is_valid_move, is_game_over, is_cats_game = board.input_move(human_move)
         while not human_is_valid_move:
             print("Invalid move")
             human_move = user_input.get_input_for_move()
-            human_is_valid_move, is_game_over = board.input_move(human_move)
+            human_is_valid_move, is_game_over, is_cats_game = board.input_move(human_move)
         print(board.board_str)
         if is_game_over:
             print("Game Over")
+        if is_cats_game:
+            print("Cat's Game")
             break
 
 if __name__ == '__main__':
